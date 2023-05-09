e716ea148b7e1aa8d5fa01a1762bebaed600e3c9
ChillerDragon <ChillerDragon@gmail.com>
2023-05-09 22:28:07 +0200

    fix vars

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 def_var 'CFG_PL_CONNECT_MAP' 'pl_connect_map' 'BlmapChill' ''
-def_var 'CFG_PL_CONNECT_MAP' 'pl_connect_cmd_prefix' 'echo hello from connect' ''
+def_var 'CFG_PL_CONNECT_CMD_PREFIX' 'pl_connect_cmd_prefix' 'echo hello from connect' ''
 
